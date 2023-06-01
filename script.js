d4a9b6006fcbb9dc7b2e5782cd55b2e0a1d4ed34
Robbie Decker <robbd56@gmail.com>
2023-06-01 17:48:18 -0400

    Fully working RPS UI
    Needs additional time to make it look nice, but functionally it works

--- script.js
+++ script.js
@@ -2,51 +2,65 @@ let options = ["rock", "paper", "scissors"];
 let userScore = 0;
 let computerScore = 0;
 
+
 // game();
-// const rock = document.getElementById("rock");
-// const paper = document.getElementById("paper");
-// const scissors = document.getElementById("scissors");
+
+const userScoreboard = document.getElementById("userScoreboard");
+const computerScoreboard = document.getElementById("computerScoreboard");
+const winCard = document.getElementById("winCard");
+const loseCard = document.getElementById("loseCard");
+const roundResult = document.getElementById("roundResult");
 
 const buttons = document.querySelectorAll('#buttons');
-console.log(buttons);
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        console.log(e.target.id);
-    });
-});
+    button.addEventListener('click',  async e => {
+        // User selects one of the three buttons
+        let userChoice = e.target.id;
+        console.log(e);
 
-rock.addEventListener('click', ()=> {
-    logName();
-});
-
-function logName(){
-    console.log(this);
-}
-
-
-// Will play a best of 5 game of Rock, Paper, Scissors
-function game(){
-    while(userScore < 3 && computerScore < 3){
-        // Show three options to user: Rock, paper, scissors
-        console.log("Please pick rock, paper, or scissors");
-        // User selects one of the three
-        let userChoice = prompt("Please pick rock, paper, or scissors");
-        userChoice = userChoice.toLowerCase();
+        // Check if user has already won
+        if(userScore >= 3 || computerScore >= 3){
+            userScore >= 3 ? alert("You already won! Press reset to play again") : alert("You already lost. Press reset to play again") ;
+            return;
+        }
 
         // Randomly choose option for computer
         let computerChoice = getComputerChoice();
-        console.log(playRound(userChoice, computerChoice));
-        showScore();
-    }
-        console.log(userScore >= 3 ? "CONGRATS! YOU BEAT THE COMPUTER" : "Sorry You Lost");
+        message = playRound(userChoice, computerChoice);
+        roundResult.textContent = message;
+        // I really want this to work before alerting the user
+        await updateScore();
+        if(userScore >= 3 || computerScore >= 3){
+            endGame();
+        }
+    });
+});
+
+const reset = document.getElementById("reset")
+reset.addEventListener('click', () =>{
+    userScore = 0;
+    computerScore = 0;
+    updateScore();
+    winCard.style.display = "none";
+    loseCard.style.display = "none" ;
+    roundResult.textContent = "";
+});
+
+
+async function updateScore(){
+    userScoreboard.textContent = `User: ${userScore}`;
+    computerScoreboard.textContent = `Computer: ${computerScore}`;
+    return;
 }
 
-function showScore(){
-    console.log(`CURRENT SCORE:\nUser: ${userScore}     Computer: ${computerScore}`);
+function endGame(){
+    userScore >= 3 ? winCard.style.display = "block" : loseCard.style.display = "block" ;
+    
 }
 
 // Battle each other and show victor
 function playRound(userChoice, computerChoice){
+    console.log(`user chose: ${userChoice}      computer chose: ${computerChoice}`);
     let message = ""
     if(computerChoice === userChoice){
         return message = "It is a draw";
